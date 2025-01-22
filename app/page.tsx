79ec74a5f37d9db5ecc817e14bb2e720d9197ba4
QUESLON VIDOTTO DE OLIVEIRA <75051018+kekovidotto@users.noreply.github.com>
2025-01-21 23:09:37 -0300

    feat: add header and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,47 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Input } from "./_components/input"
 import { Button } from "./_components/ui/button"
+import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
 
 export default function Home() {
-  return <Button>Button</Button>
+  return (
+    <div>
+      {/* Header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Keko</h2>
+        <p>Segunda-feira, 05 de agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            className="rounded-xl object-cover"
+            src="/banner-01.png"
+            fill
+            alt="Agende nos melhores com FSW Barber"
+          />
+        </div>
+
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex">
+            {/*ESQUERDA*/}
+            <div className="flex flex-col gap-2 py-5">
+              <Badge>Confirmado</Badge>
+              <h3>Corte de Cabelo</h3>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
 }
