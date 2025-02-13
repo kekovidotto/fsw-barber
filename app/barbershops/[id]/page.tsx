a53379164ee739cca305bb97ace19f24e73305f2
QUESLON VIDOTTO DE OLIVEIRA <75051018+kekovidotto@users.noreply.github.com>
2025-02-13 20:25:05 -0300

    feat: remove type params

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -9,13 +9,13 @@ import Image from "next/image"
 import Link from "next/link"
 import { notFound } from "next/navigation"
 
-// interface BarbershopPageProps {
-//   params: {
-//     id: string
-//   }
-// }
+interface BarbershopPageProps {
+  params: {
+    id: string
+  }
+}
 
-const BarbershopPage = async ({ params }: { params: { id: string } }) => {
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id,
@@ -63,7 +63,7 @@ const BarbershopPage = async ({ params }: { params: { id: string } }) => {
         </Sheet>
       </div>
 
-      {/* INFORMAÇÕES */}
+      {/* INFORMAÇÕES */}
       <div className="border-b border-solid p-5">
         <h1 className="mb-3 text-xs font-bold">{barbershop.name}</h1>
         <div className="mb-2 flex items-center gap-2">
@@ -76,9 +76,9 @@ const BarbershopPage = async ({ params }: { params: { id: string } }) => {
         </div>
       </div>
 
-      {/* DESCRIÇÂO */}
+      {/* DESCRIÇÃO */}
       <div className="space-y-3 border-b border-solid p-5">
-        <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
+        <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop.description}</p>
       </div>
 
